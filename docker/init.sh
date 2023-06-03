cbdfc7e4ec4769522f4e1251d9b66c4107350b62
lolivier <github@levillain.ml>
2023-06-03 14:50:23 +0200

    setup for docker

--- docker/init.sh
+++ docker/init.sh
@@ -3,6 +3,14 @@
 # create the sqlite3 database if it does not exist yet
 DB=/sqlite3/cousinsmatter.db
 SCHEMA=/sqlite3/schema.sql
-test -f $DB || sqlite3 $DB < $SCHEMA
-
-exec docker-php-entrypoint "$@"
+echo 'checking if sqlite3 db exists...'
+if [ ! -f $DB ]; then
+	echo 'creating empty database'
+	sqlite3 $DB < $SCHEMA
+	chown www-data:www-data $DB
+	chmod ug+rwx /sqlite3
+	chmod ug+rw $DB
+fi
+echo 'initialized sqlite3 db'
+echo 'starting docker php entry point with apache2-foreground '"$@"
+exec docker-php-entrypoint apache2-foreground "$@"
